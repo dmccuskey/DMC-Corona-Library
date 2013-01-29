79f3b5a24220c3157b7211c14ade09f4b2711b87
David McCuskey <my-github@davidmccuskey.com>
2013-01-28 22:51:06 -0800

    copy changes from work area

--- dmc_autostore.lua
+++ dmc_autostore.lua
@@ -32,14 +32,14 @@ DEALINGS IN THE SOFTWARE.
 
 -- Semantic Versioning Specification: http://semver.org/
 
-local VERSION = "0.1.0"
+local VERSION = "1.0.0"
 
 
 --====================================================================--
 -- Imports
 --====================================================================--
 
-local json = require ( "json" )
+local json = require( "json" )
 
 
 
@@ -59,6 +59,42 @@ local AutoStore
 local autostore_singleton = nil
 local TableProxy
 local createTableProxy
+
+
+
+--====================================================================--
+-- Support Methods
+--====================================================================--
+
+
+-- extend()
+-- copied from DMC Utils
+-- only used during event dispatch
+--
+function extend( fromTable, toTable )
+
+	function _extend( fT, tT )
+
+		for k,v in pairs( fT ) do
+
+			if type( fT[ k ] ) == "table" and
+				type( tT[ k ] ) == "table" then
+
+				tT[ k ] = _extend( fT[ k ], tT[ k ] )
+
+			elseif type( fT[ k ] ) == "table" then
+				tT[ k ] = _extend( fT[ k ], {} )
+
+			else
+				tT[ k ] = v
+			end
+		end
+
+		return tT
+	end
+
+	return _extend( fromTable, toTable )
+end
 
 
 
@@ -238,6 +274,7 @@ function TableProxy:len()
 	--print( "TableProxy:len" )
 	local mt = getmetatable( self )
 	local t = mt.__dmc.t
+
 	return #t
 end
 
@@ -262,9 +299,23 @@ end
 
 function TableProxy:pairs()
 	--print( "TableProxy:pairs" )
+
 	local mt = getmetatable( self )
 	local t = mt.__dmc.t
-	return pairs( t )
+
+	-- custom iterator
+	-- @param tp ref: TableProxy (ie, self)
+	-- @param key string: key of previous item
+	local f = function( tp, k )
+		local key,_ = next( t, k )
+		if key ~= nil then
+			return key,tp[key]
+		else
+			return nil
+		end
+	end
+
+	return f, self, nil
 end
 
 function TableProxy:insert( value, pos )
@@ -324,20 +375,37 @@ createTableProxy = function( table_obj )
 end
 
 
+
 --====================================================================--
 -- Auto Store Class
 --====================================================================--
 
-AutoStore = {}
+local AutoStore = {}
 
 -- NOTE: defaults with upper case names are not copied !!!
 --
 AutoStore.DEFAULTS = {
 	CONFIG_FILE = 'dmc_autostore.cfg',
-	data_file = 'dmc_autostore.json',
-	timer_min = 2000,
-	timer_max = 6000
+	data_filename = { type='string', value='dmc_autostore' }, -- '.json' appended later
+	timer_min = { type='integer', value=1000 },
+	timer_max = { type='integer', value=4000 }
 }
+
+
+-- keyed on callback function
+AutoStore._eventListeners = {}
+
+
+-- Event Name
+AutoStore.AUTOSTORE_EVENT = 'autostore_event'
+
+-- Event Types
+AutoStore.START_MIN_TIMER = 'start_min_timer'
+AutoStore.STOP_MIN_TIMER = 'stop_min_timer'
+AutoStore.START_MAX_TIMER = 'start_max_timer'
+AutoStore.STOP_MAX_TIMER = 'stop_max_timer'
+AutoStore.DATA_SAVED = 'data_saved'
+
 
 function AutoStore:new()
 	--print( "AutoStore:new" )
@@ -371,7 +439,7 @@ function AutoStore:init()
 	for k, v in pairs( AutoStore.DEFAULTS ) do
 		-- if uppercase, then don't include
 		if k == string.lower( k ) then
-			self._config[ k ] = v
+			self._config[ k ] = v.value
 		end
 	end
 
@@ -387,11 +455,14 @@ function AutoStore:init()
 			is_valid = ( string.find( line, '--', 1, true ) ~= 1 )
 
 			if is_valid then
-				for k, v in string.gmatch( line, "([%w_]+)%s*=%s*(%w+)" ) do
+				for k, v in string.gmatch( line, "([%w_]+)%s*=%s*([%w_]+)" ) do
 					--print( tostring( k ) .. " = " .. tostring( v ) )
-					v = tonumber( v )
-					if v == nil then v = 0 end
+
 					k = string.lower( k ) -- use only lowercase inside of module
+					if AutoStore.DEFAULTS[ k ].type == 'integer' then
+						v = tonumber( v )
+						if v == nil then v = 0 end
+					end
 					self._config[ k ] = v
 				end
 			end
@@ -399,8 +470,11 @@ function AutoStore:init()
 	end
 
 
-	-- check
+	-- container for event listeners
+	self._eventListeners[ AutoStore.AUTOSTORE_EVENT ] = {}
 
+
+	-- check
 	-- timer_min can't be <= timer_max
 	-- TODO: sanity check on timers
 
@@ -409,7 +483,7 @@ end
 function AutoStore:load()
 	--print( "AutoStore:load" )
 
-	local file_path = system.pathForFile( self._config.data_file, system.DocumentsDirectory )
+	local file_path = system.pathForFile( self._config.data_filename .. '.json', system.DocumentsDirectory )
 	local status, content = readFile( file_path, { lines=false } )
 
 	if status == Response.ERROR then
@@ -426,11 +500,13 @@ end
 function AutoStore:save()
 	--print( "AutoStore:save" )
 
-	local file_path = system.pathForFile( self._config.data_file, system.DocumentsDirectory )
+	local file_path = system.pathForFile( self._config.data_filename .. '.json', system.DocumentsDirectory )
 	local json = json.encode( self.data:__data() )
 	local status, content = saveFile( file_path, json )
 
 	self.is_new_file = false
+
+	self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.DATA_SAVED )
 
 end
 
@@ -442,33 +518,69 @@ function AutoStore:isDirty()
 	-- end any current timer
 	if self._timer_min ~= nil then
 		timer.cancel( self._timer_min )
+		self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.STOP_MIN_TIMER )
+
 	end
 
 	-- setup minimum timer
 	f = function()
 		if self._timer_max ~= nil then
 			timer.cancel( self._timer_max )
+			self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.STOP_MAX_TIMER )
 			self._timer_max = nil
 		end
 		self._timer_min = nil
 		self:save()
 	end
 	self._timer_min = timer.performWithDelay( self._config.timer_min, f )
+	self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.START_MIN_TIMER, { time=self._config.timer_min } )
 
 	-- setup maximum timer
 	if self._timer_max == nil then
 		f = function()
 			if self._timer_min ~= nil then
 				timer.cancel( self._timer_min )
+				self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.STOP_MIN_TIMER )
 				self._timer_min = nil
 			end
 			self._timer_max = nil
 			self:save()
 		end
 		self._timer_max = timer.performWithDelay( self._config.timer_max, f )
+		self:dispatchEvent( AutoStore.AUTOSTORE_EVENT, AutoStore.START_MAX_TIMER, { time=self._config.timer_max } )
 	end
 end
 
+
+function AutoStore:dispatchEvent( event, type, data )
+	--print( "AutoStore:dispatchEvent" )
+
+	local et = self._eventListeners[ event ]
+
+	local e = {
+		name=event,
+		type=type
+	}
+
+	if data ~= nil then e = extend( data, e ) end
+
+	for _, data in pairs( et ) do
+		data[2]( e )
+	end
+
+end
+
+function AutoStore:addEventListener( type, callback )
+	--print( "AutoStore:addEventListener" )
+
+	local o = self._eventListeners[ type ]
+	if self._eventListeners[ type ] == nil then
+		print( "ERROR")
+	else
+		o[ callback ] = { type, callback }
+	end
+
+end
 
 
 if autostore_singleton == nil then
