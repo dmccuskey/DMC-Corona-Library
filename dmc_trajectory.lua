7b32f970530d86c7b55a4aa194369d0123daf45c
David McCuskey <my-github@davidmccuskey.com>
2013-02-02 16:38:36 -0800

    update copyright on library items

--- dmc_trajectory.lua
+++ dmc_trajectory.lua
@@ -7,7 +7,7 @@
 
 --[[
 
-Copyright (C) 2012 David McCuskey. All Rights Reserved.
+Copyright (C) 2012-2013 David McCuskey. All Rights Reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in the
