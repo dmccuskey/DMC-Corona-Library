827246c3d955374c6222b5fd6abbaa082b836566
David McCuskey <my-github@davidmccuskey.com>
2013-06-17 17:06:24 -0700

    increment version number, 1.0.2

--- dmc_objects.lua
+++ dmc_objects.lua
@@ -32,7 +32,7 @@ DEALINGS IN THE SOFTWARE.
 
 -- Semantic Versioning Specification: http://semver.org/
 
-local VERSION = "1.0.1"
+local VERSION = "1.0.2"
 
 
 -- =========================================================
