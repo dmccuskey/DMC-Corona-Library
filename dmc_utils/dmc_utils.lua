5a40c434e01643aae3dd246ebe7acb16c6f3deb8
David McCuskey <my-github@davidmccuskey.com>
2013-12-20 14:04:52 -0700

    add dmc lib boot scripts

--- dmc_utils/dmc_utils.lua
+++ dmc_utils/dmc_utils.lua
@@ -36,12 +36,110 @@ local VERSION = "0.8.1"
 
 
 
---====================================================================--
--- Setup
---====================================================================--
 
+--====================================================================--
+-- DMC Library Support Methods
+--====================================================================--
 
 local Utils = {}
+
+function Utils.extend( fromTable, toTable )
+
+	function _extend( fT, tT )
+
+		for k,v in pairs( fT ) do
+
+			if type( fT[ k ] ) == "table" and
+				type( tT[ k ] ) == "table" then
+
+				tT[ k ] = _extend( fT[ k ], tT[ k ] )
+
+			elseif type( fT[ k ] ) == "table" then
+				tT[ k ] = _extend( fT[ k ], {} )
+
+			else
+				tT[ k ] = v
+			end
+		end
+
+		return tT
+	end
+
+	return _extend( fromTable, toTable )
+end
+
+
+
+--====================================================================--
+-- DMC Library Config
+--====================================================================--
+
+local dmc_lib_data, dmc_lib_info, dmc_lib_location
+
+-- boot dmc_library with boot script or
+-- setup basic defaults if it doesn't exist
+--
+if false == pcall( function() require( "dmc_library_boot" ) end ) then
+	_G.__dmc_library = {
+		dmc_library={
+			location = ''
+		},
+		func = {
+			find=function( name )
+				local loc = ''
+				if dmc_lib_data[name] and dmc_lib_data[name].location then
+					loc = dmc_lib_data[name].location
+				else
+					loc = dmc_lib_info.location
+				end
+				if loc ~= '' and string.sub( loc, -1 ) ~= '.' then
+					loc = loc .. '.'
+				end
+				return loc .. name
+			end
+		}
+	}
+end
+
+dmc_lib_data = _G.__dmc_library
+dmc_lib_func = dmc_lib_data.func
+dmc_lib_info = dmc_lib_data.dmc_library
+dmc_lib_location = dmc_lib_info.location
+
+
+
+
+--====================================================================--
+-- DMC Library : DMC Utils
+--====================================================================--
+
+
+
+
+--====================================================================--
+-- DMC Utils Config
+--====================================================================--
+
+dmc_lib_data.dmc_utils = dmc_lib_data.dmc_utils or {}
+
+local DMC_UTILS_DEFAULTS = {
+	-- none
+}
+
+local dmc_utils_data = Utils.extend( dmc_lib_data.dmc_utils, DMC_UTILS_DEFAULTS )
+
+
+
+-- =========================================================
+-- Imports
+-- =========================================================
+
+
+
+--====================================================================--
+-- Module Functions
+--====================================================================--
+
 
 
 -- extend()
