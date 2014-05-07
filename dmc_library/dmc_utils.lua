5cc78b739902632c590618ff14315ad9db44e7da
David McCuskey <my-github@davidmccuskey.com>
2014-05-07 12:07:34 -0600

    add string split() function

--- dmc_library/dmc_utils.lua
+++ dmc_library/dmc_utils.lua
@@ -516,5 +516,26 @@ end
 
 
 
+--====================================================================--
+-- String Functions
+--====================================================================--
+
+
+-- split string up in parts, using separator
+-- returns array of pieces
+function Utils.split( str, sep )
+	if sep == nil then
+		sep = "%s"
+	end
+	t={} ; i=1
+	for str in string.gmatch( str, "([^"..sep.."]+)") do
+		t[i] = str
+		i = i + 1
+	end
+	return t
+end
+
+
+
 
 return Utils
