1afd5044820366587aba56859f46312baa5ac062
David McCuskey <my-github@davidmccuskey.com>
2014-05-14 10:03:03 -0600

    update copyright

--- dmc_library/dmc_objects.lua
+++ dmc_library/dmc_objects.lua
@@ -1,4 +1,3 @@
-
 --====================================================================--
 -- dmc_objects.lua
 --
@@ -8,7 +7,7 @@
 
 --[[
 
-Copyright (C) 2011-2013 David McCuskey. All Rights Reserved.
+Copyright (C) 2011-2014 David McCuskey. All Rights Reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in the
