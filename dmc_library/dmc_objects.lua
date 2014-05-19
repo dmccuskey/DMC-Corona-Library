fdcc67dec6270565cb0735aea5fa748281dbe2e8
David McCuskey <my-github@davidmccuskey.com>
2014-05-19 16:18:55 -0600

    issue #26, add ObjectBase class to dmc_objects

--- dmc_library/dmc_objects.lua
+++ dmc_library/dmc_objects.lua
@@ -494,10 +494,237 @@ function Object:createCallback( method )
 end
 
 
--- =========================================================
--- CoronaBase Class
--- =========================================================
 
+--====================================================================--
+-- Object Base Class
+--====================================================================--
+
+local ObjectBase = inheritsFrom( Object )
+ObjectBase.NAME = "Object Base"
+
+
+--====================================================================--
+--== Start: Setup DMC Objects
+
+-- _init()
+-- initialize the object - setting the view
+--
+function ObjectBase:_init( options )
+	-- OVERRIDE THIS
+	--== Create Properties ==--
+	self.__event_listeners = {} -- holds event listeners
+	--[[
+	event listeners key'd by:
+	* <event name>::<function>
+	* <event name>::<object>
+	{
+		<event name> = {
+			'event::function' = func,
+			'event::object' = object (table)
+		}
+	}
+	--]]
+	--== Object References ==--
+end
+-- _undoInit()
+-- remove items added during _init()
+--
+function ObjectBase:_undoInit( options )
+	-- OVERRIDE THIS
+	self.__event_listeners = nil
+end
+
+
+-- _initComplete()
+-- any setup after object is done being created
+--
+function ObjectBase:_initComplete()
+	-- OVERRIDE THIS
+end
+-- _undoInitComplete()
+-- remove any items added during _initComplete()
+--
+function ObjectBase:_undoInitComplete()
+	-- OVERRIDE THIS
+end
+
+--== END: Setup DMC Objects
+--====================================================================--
+
+
+
+--====================================================================--
+--== Public Methods
+
+-- new()
+-- class constructor
+--
+function ObjectBase:new( params )
+	params = params or {}
+	--==--
+
+	local o = self:_bless()
+
+	-- set flag if this is an Intermediate class
+	if params.__setIntermediate == true then
+		o.is_intermediate = true
+		params.__setIntermediate = nil
+	else
+		o.is_intermediate = false
+	end
+
+	o:_init( params )
+
+	-- skip these if we're an intermediate class (eg, subclass)
+	if rawget( o, 'is_intermediate' ) == false then
+		o:_initComplete()
+	end
+
+	return o
+end
+
+-- addEventListener()
+--
+function ObjectBase:addEventListener( e_name, listener )
+	-- print( "ObjectBase:addEventListener", e_name, listener );
+
+	-- Sanity Check
+
+	if not e_name or type(e_name)~='string' then
+		error( "ERROR addEventListener: event name must be string", 2 )
+	end
+	if not listener and not Utils.propertyIn( {'function','table'}, type(listener) ) then
+		error( "ERROR addEventListener: listener must be a function or object", 2 )
+	end
+
+	-- Processing
+
+	local events, listeners, key
+
+	events = self.__event_listeners
+	if not events[ e_name ] then events[ e_name ] = {} end
+	listeners = events[ e_name ]
+
+	key = self:_createEventListenerKey( e_name, listener )
+	if listeners[ key ] then
+		print("WARNING:: ObjectBase:addEventListener, already have listener")
+	else
+		listeners[ key ] = listener
+	end
+
+end
+
+-- dispatchEvent( event, data, params )
+--
+function ObjectBase:dispatchEvent( e_type, data, params )
+	-- print( "ObjectBase:dispatchEvent", e_type );
+	params = params or {}
+	if params.merge == nil then params.merge = false end
+	--==--
+	local e
+
+	if params.merge and type( data ) == 'table' then
+		e = data
+		e.name = self.EVENT
+		e.type = e_type
+		e.target = self
+
+	else
+		e = {
+			name=self.EVENT,
+			type=e_type,
+			target=self,
+			data=data
+		}
+
+	end
+
+	self:_dispatchEvent( e )
+end
+
+-- removeEventListener()
+--
+function ObjectBase:removeEventListener( e_name, listener )
+	-- print( "ObjectBase:removeEventListener" );
+
+	local listeners, key
+
+	listeners = self.__event_listeners[ e_name ]
+	if not listeners or type(listeners)~= 'table' then
+		print("WARNING:: ObjectBase:removeEventListener, no listeners found")
+	end
+
+	key = self:_createEventListenerKey( e_name, listener )
+
+	if not listeners[ key ] then
+		print("WARNING:: ObjectBase:removeEventListener, listener not found")
+	else
+		listeners[ key ] = nil
+	end
+
+end
+
+-- removeSelf()
+--
+function ObjectBase:removeSelf()
+	-- print( "ObjectBase:removeSelf" );
+
+	-- skip these if we're an intermediate class (eg, subclass)
+	if rawget( self, 'is_intermediate' ) == false then
+		self:_undoInitComplete()
+	end
+
+	self:_undoInit()
+end
+
+
+--====================================================================--
+--== Private Methods
+
+-- callback is either function or object (table)
+function ObjectBase:_createEventListenerKey( e_name, callback )
+	return e_name .. "::" .. tostring( callback )
+end
+
+
+function ObjectBase:_dispatchEvent( event )
+	-- print( "ObjectBase:_dispatchEvent", event.name );
+	local e_name, listeners
+
+	e_name = event.name
+	if not e_name or not self.__event_listeners[ e_name ] then return end
+
+	listeners = self.__event_listeners[ e_name ]
+	if type(listeners)~='table' then return end
+
+	for k, callback in pairs( listeners ) do
+
+		if type( callback) == 'function' then
+		 	callback( event )
+
+		elseif type( callback )=='table' and callback[e_name] then
+			local method = callback[e_name]
+			method( callback, event )
+
+		else
+			print( "WARNING: ObjectBase dispatchEvent", e_name )
+
+		end
+	end
+end
+
+
+--====================================================================--
+--== Event Handlers
+
+-- none
+
+
+
+
+--====================================================================--
+-- CoronaBase Class
+--====================================================================--
 
 local CoronaBase = inheritsFrom( Object )
 CoronaBase.NAME = "Corona Base"
@@ -1165,6 +1392,7 @@ end
 local Objects = {
 	inheritsFrom = inheritsFrom,
 	Object = Object,
+	ObjectBase = ObjectBase,
 	CoronaBase = CoronaBase,
 	CoronaPhysics = CoronaPhysics,
 }
