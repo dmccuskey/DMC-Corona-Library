7185bfeaed97253555a9f5d9c5c3d3bc130de8df
David McCuskey <my-github@davidmccuskey.com>
2014-05-19 16:55:56 -0600

    update version dmc_sockets.atcp 0.1.3

--- dmc_library/dmc_sockets/atcp.lua
+++ dmc_library/dmc_sockets/atcp.lua
@@ -32,8 +32,7 @@ DEALINGS IN THE SOFTWARE.
 
 -- Semantic Versioning Specification: http://semver.org/
 
-local VERSION = "0.1.2"
-
+local VERSION = "0.1.3"
 
 
 --====================================================================--
