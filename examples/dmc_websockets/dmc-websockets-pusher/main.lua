11548d8a233de3ea93537340008f710f660a5890
David McCuskey <my-github@davidmccuskey.com>
2014-07-18 13:15:11 -0600

    clean up main.lua

--- examples/dmc_websockets/dmc-websockets-pusher/main.lua
+++ examples/dmc_websockets/dmc-websockets-pusher/main.lua
@@ -28,31 +28,7 @@ local ws
 --====================================================================--
 -- Support Functions
 
-local function sendMessages()
-
-	local num_msgs = 5
-	local count = 0
-
-	print("=== Sending " .. tostring( num_msgs ) .. " messages ===\n")
-
-	local f = function()
-
-		count = count + 1
-		local str = "Current app time: " .. tostring( system.getTimer() )
-		print( "Sending message (" .. tostring( count ) .. "): '" .. str .. "'")
-
-		-- ws:_sendPing()
-		ws:send( str )
-
-		if count == num_msgs then
-			timer.performWithDelay( 1500, function() print( "Closing WebSocket connection" ); ws:close() end )
-		end
-
-	end
-
-	timer.performWithDelay( 1000, f, num_msgs )
-
-end
+-- none
 
 
 --====================================================================--
@@ -64,13 +40,12 @@ local function webSocketsEvent_handler( event )
 
 	if evt_type == ws.ONOPEN then
 		print( 'Received event: ONOPEN' )
-		-- sendMessages()
 
 	elseif evt_type == ws.ONMESSAGE then
 		local msg = event.message
 
 		print( "Received event: ONMESSAGE" )
-		print( "echoed message: '" .. tostring( msg.data ) .. "'\n\n" )
+		print( "message: '" .. tostring( msg.data ) .. "'\n\n" )
 
 	elseif evt_type == ws.ONCLOSE then
 		print( "Received event: ONCLOSE" )
@@ -78,7 +53,6 @@ local function webSocketsEvent_handler( event )
 
 	elseif evt_type == ws.ONERROR then
 		print( "Received event: ONERROR" )
-		-- Utils.print( event )
 
 	end
 end
