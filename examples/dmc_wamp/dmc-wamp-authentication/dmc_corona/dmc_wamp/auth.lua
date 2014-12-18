97aa6e739f27ba25707fe92103ee0aaa17b58a39
D McCuskey <my-github@davidmccuskey.com>
2014-12-18 11:34:48 -0700

    stub rest of functions in auth

--- examples/dmc_wamp/dmc-wamp-authentication/dmc_corona/dmc_wamp/auth.lua
+++ examples/dmc_wamp/dmc-wamp-authentication/dmc_corona/dmc_wamp/auth.lua
@@ -50,14 +50,101 @@ local mbase64_encode = mime.b64
 
 
 --====================================================================--
+--== Setup, Constants
+
+
+-- The characters from which :func:`autobahn.wamp.auth.generate_wcs`
+-- generates secrets
+local WCS_SECRET_CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+
+
+--====================================================================--
 --== Support Functions
+
+
+local function generate_totp_secret( length )
+	-- print( "dmc_wamp.auth.generate_totp_secret ", length )
+	--==--
+	length = length or 10
+
+	assert( type( length ) == 'number' )
+
+	error( "not implemented" )
+end
+
+
+local function compute_totp( secret, offset )
+	-- print( "dmc_wamp.auth.compute_totp ", secret, offset )
+	--==--
+	offset = offset or 0
+
+	assert( type( secret ) == 'string' )
+	assert( type( length ) == 'number' )
+
+	error( "not implemented" )
+end
+
+
+
+local function pbkdf2( params )
+	-- print( "dmc_wamp.auth.pbkdf2 ", params )
+	params = params or {}
+	--==--
+	local data, salt = params.data, params.salt
+	local iterations = params.iterations or 1000
+	local keylen = params.keylen or 32
+	local hashfunc = params.hashfunc or crypto.sha256
+
+	assert( type( data ) == 'string' )
+	assert( type( salt ) == 'string' )
+	assert( type( iterations ) == 'number' )
+	assert( type( keylen ) == 'number' )
+	assert( type( hashfunc ) == 'function' )
+
+	-- return _pdkdf2( data, salt, iterations, keylen, hashfunc )
+	error( "not implemented" )
+
+end
+
+
+
+local function derive_key( params )
+	-- print( "dmc_wamp.auth.derive_key ", params )
+	params = params or {}
+	--==--
+	local secret, salt = params.secret, params.salt
+	local iterations = params.iterations or 1000
+	local keylen = params.keylen or 32
+
+	assert( type( secret ) == 'string' )
+	assert( type( salt ) == 'string' )
+	assert( type( iterations ) == 'number' )
+	assert( type( keylen ) == 'number' )
+
+	error( "not implemented" )
+end
+
+
+
+local function generate_wcs( length )
+	-- print( "dmc_wamp.auth.generate_wcs ", length )
+	--==--
+	length = length or 10
+
+	assert( type( length ) == 'number' )
+
+	error( "not implemented" )
+end
+
 
 
 local function compute_wcs( key, challenge )
 	-- print( "dmc_wamp.auth.compute_wcs ", key, challenge )
-	assert( type(key)=='string' )
-	assert( type(challenge)=='string' )
 	--==--
+	assert( type( key ) == 'string' )
+	assert( type( challenge ) == 'string' )
+
 	local sig = crypto.hmac( crypto.sha256, challenge, key, true )
 	return mbase64_encode( sig )
 end
@@ -71,6 +158,11 @@ end
 
 
 return {
+	generate_totp_secret=generate_totp_secret,
+	compute_totp=compute_totp,
+	pbkdf2=pbkdf2,
+	derive_key=derive_key,
+	generate_wcs=generate_wcs,
 	compute_wcs=compute_wcs
 }
 
