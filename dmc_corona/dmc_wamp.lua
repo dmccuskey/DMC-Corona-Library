22b6b2d6cd578d711514c94cabb5d0d136f45c11
dmc <my-github@davidmccuskey.com>
2015-02-10 18:11:49 -0700

    update module libs

--- dmc_corona/dmc_wamp.lua
+++ dmc_corona/dmc_wamp.lua
@@ -168,6 +168,11 @@ local Wamp = newClass( { WebSocket, StatesMix }, { name="WAMP Connector" } )
 
 Wamp.DEFAULT_PROTOCOL = { 'wamp.2.json' }
 
+-- Auth Types
+
+Wamp.AUTH_WAMPCRA = 'wampcra'
+Wamp.AUTH_TICKET = 'ticket'
+
 --== Event Constants ==--
 
 Wamp.EVENT = 'wamp_event'
@@ -254,6 +259,27 @@ end
 
 --====================================================================--
 --== Public Methods
+
+
+-- user_id, setter, string
+--
+function Wamp.__setters:user_id( value )
+	-- print( "Wamp.__setters:user_id", value )
+	assert( type(value)=='string' )
+	--==--
+	self._config.authid = value
+end
+
+-- auth_methods, setter, table of auth strings
+--
+function Wamp.__setters:auth_methods( value )
+	-- print( "Wamp.__setters:auth_methods", value )
+	assert( type(value)=='table' )
+	--==--
+	self._config.authmethods = value
+end
+
+
 
 
 -- is_connected, getter, boolean
